7e67e4461e236f4dc097647b391824d64d898913
Rodrigo <godites@live.com>
2020-09-03 11:58:19 -0500

    menu eleccion de ejercitos

--- src/Menu.java
+++ src/Menu.java
@@ -1,11 +1,17 @@
 public class Menu {
+    
     FabricaEjercitos fabricaEjercitos;
     Ejercito ejercito;
 
-    // en progreso, se supone que seria Facade
+    
     public void MenuEjercito(int i) {
         System.out.println("Escoge un ejercito, se muestra la informacion de cada uno...");
-        System.out.println();
+        System.out.println(
+                "1 - EJERCITO EXPLORADOR: Se conforma con 3 pelotones.\nPelotón 1 tienen 6 soldados de infantería.\nPelotón 2 se compone con 1 artillero y 2 miembros de la caballería.\nPelotón 3 se compone con 6 soldados, todos miembros de la caballería.");
+        System.out.println(
+                "2 - EJERCITO DEFAULT: Se conforma con 3 pelotones.\nPelotón 1 tienen 6 soldados, todos miembros de la infantería.\nPelotón 2 tiene 3 artilleros.\nPelotón 3 tiene 6 miembros de la caballería.");
+        System.out.println(
+                "3 - EJERCITO KAMIKAZE: Se conforma de 3 pelotones.\nPelotón 1 y 2 tienen 5 soldados cada uno, todos miembros de la infantería.\nPelotón 3 tiene 5 soldados, todos miembros de la caballería.");
         switch (i) {
             case 1:
                 ejercito = fabricaEjercitos.getEjercito("explorador");
@@ -17,9 +23,9 @@ public class Menu {
                 ejercito = fabricaEjercitos.getEjercito("kamikaze");
                 break;
             default:
+                System.out.println("No es una opcion valida, vuelve a escoger...");
                 break;
         }
     }
 
-    
 }
